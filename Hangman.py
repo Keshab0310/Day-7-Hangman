bd9a01e3dc5207c51834cb39ba6941e09cc90727
Keshab Kumar Pathak <156252352+Keshab0310@users.noreply.github.com>
2024-04-25 14:58:25 -0400

    Done with more clean code as well as explaining all the stuffs that has been added

--- Hangman.py
+++ Hangman.py
@@ -1,22 +1,32 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-choosen_word = random.choice(word_list)
-word_length = len(choosen_word)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
 display = []
-
 for _ in range(word_length):
     display += "_"
-print(display)
 
+#Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
 
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-guess = input("Guess a letter: ").lower()
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+       
+        if letter == guess:
+            display[position] = letter
 
-for position in range(word_length):
-    letter = choosen_word[position]
-    if letter == guess:
-        display[position] = letter
+    print(display)
 
-print(display)
-
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
